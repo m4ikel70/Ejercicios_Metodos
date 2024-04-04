cbb376fad2c7222f395e1f5c7e7154d5803f372e
m4ikel70 <masanchezg78@educastillalamancha.es>
2024-04-04 09:32:08 +0200

    Documentación de Javadoc para el metodo existe

--- src/main/java/es/dam1m/ejercicios_metodos/FUtil.java
+++ src/main/java/es/dam1m/ejercicios_metodos/FUtil.java
@@ -3,8 +3,14 @@ package es.dam1m.ejercicios_metodos;
 import java.io.File; 
  
 public class FUtil { 
-  
-	 public static boolean existe(String filename) { 
+          
+    /**
+     * Comprueba si un archivo existe mediante un parametro.
+     * @param filename el nombre del archivo que se quiere comprobar
+     * @return true si el archivo existe, false si no existe
+     * @throws IllegalArgumentException si la cadena está vacía o contiene espacios en blanco
+     */
+    	 public static boolean existe(String filename) { 
             if (filename == null || filename.trim().isEmpty()) {
                 throw new IllegalArgumentException
                 ("El nombre introducido esta en blanco "
